82d29a71fddd197dac04ed64ac46bed5eecf5474
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2020-05-26 11:02:41 +0530

    added ajax code to contact-from js

--- js/contact-form.js
+++ js/contact-form.js
@@ -29,6 +29,15 @@ Ajax Contact Form
             'message' : $('textarea[name="form-message"]').val()
         };
 
+        $("#my-form").submit(function(e) {
+            e.preventDefault();
+          
+            var $form = $(this);
+            $.post($form.attr("action"), $form.serialize()).then(function() {
+              alert("Thank you!");
+            });
+          });
+
         // process the form
         $.ajax({
             type : 'POST',
